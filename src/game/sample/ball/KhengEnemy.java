854d63bc266704b94de7f76fd9d5869ab2e85e37
Mahdi <mahdighajary@gmail.com>
2018-07-09 18:35:07 +0430

    one type of moving enemy

---
+++ src/game/sample/ball/KhengEnemy.java
@@ -0,0 +1,8 @@
+package game.sample.ball;
+
+public class KhengEnemy extends MovingEnemy {
+    public KhengEnemy() {
+        super("constantEnemy.png", 25, 55, 85, 75,
+                0.12,1,100);
+    }
+}

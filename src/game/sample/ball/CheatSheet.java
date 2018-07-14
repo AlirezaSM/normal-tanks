1b254c422c80e732f3a5c61ae15966e560808127
Mahdi <mahdighajary@gmail.com>
2018-07-14 20:08:22 +0430

    cheatsheet

---
+++ src/game/sample/ball/CheatSheet.java
@@ -0,0 +1,38 @@
+package game.sample.ball;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CheatSheet extends JFrame {
+    JTextField jtf = new JTextField(30);
+    JButton jb = new JButton("Cheat And Chill!");
+
+    public CheatSheet(GameState state) {
+        setSize(300, 300);
+        setLayout(null);
+        jb.setBounds(70,150,125,45);
+        add(jb);
+        jtf.setBounds(20,50,250,40);
+        jtf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getActionCommand().equalsIgnoreCase("health")) {
+                    state.mainTankHealth+= 300;
+                }
+                else if (e.getActionCommand().equalsIgnoreCase("cannon")) {
+                    state.numOfHeavyBullets+= 100;
+                }
+                else if (e.getActionCommand().equalsIgnoreCase("machineGun")) {
+                    state.numOfMachineGunBullets+= 1000;
+                }
+                else if (e.getActionCommand().equalsIgnoreCase("health")) {
+                    state.mainTankHealth+= 300;
+                }
+            }
+        });
+        add(jtf);
+        setVisible(true);
+    }
+}
